a351cac4bfeb82223dfe1ed82b433aaf3f61c897
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-16 22:46:43 +0530

    Vidly Movie Database

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Movies from './component/movies'
+
+function App() {
+  return (
+    <div className="App">
+     <Movies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/movies.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {getMovies} from '../services/fakeMovieService'
+export default class movies extends Component {
+    state={
+        movies:getMovies()
+    };
+    handelDelete= movie=>{
+        const movies=this.state.movies.filter(m => m._id!==movie._id)
+        this.setState({movies})
+    }
+    render() {
+        const {length:count}=this.state.movies;
+        if (count ===0) return <p>There are no movies in the database</p>
+        return (
+            <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map((movies)=>(
+                  <tr key={movies._id}>
+                    <td>{movies.title}</td>
+                    <td>{movies.genre.name}</td>
+                    <td>{movies.numberInStock}</td>
+                    <td>{movies.dailyRentalRate}</td>
+                    <td><button className="btn btn-danger btn-sm" onClick={()=>
+                        this.handelDelete(movies)
+                    }>Delete</button></td>
+                </tr>  
+                ))}
+                
+            </tbody>
+        </table>
+        </React.Fragment>
+        )}
+}
