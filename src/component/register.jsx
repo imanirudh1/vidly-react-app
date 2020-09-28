0866b4fa54d3cc70020718e1104a0050b50db685
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-28 21:26:27 +0530

    Added userService to make a request on the server

--- src/component/register.jsx
+++ src/component/register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-
+import * as userService from '../services/userService'
 class Register extends Form {
   state = {
     data: {
@@ -20,8 +20,17 @@ class Register extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
-    console.log("Registered");
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data)
+    }
+    catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const error={...this.state.error}
+        error.email =ex.response.data
+        this.setState({error})
+      }
+    }
   };
 
   render() {
@@ -30,7 +39,7 @@ class Register extends Form {
         <h1>Register</h1>
         <form onSubmit={this.formHandeler}>
           {this.renderInput("email", "Email")}
-          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("password", "Password", "password")}   
           {this.renderInput("name", "Name")}
           {this.renderButton("Register")}
         </form>
